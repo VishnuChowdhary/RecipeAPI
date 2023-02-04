37509e880d3ced2cee5a3c9b9d30c4fb97701988
VishnuChowdhary <112419086+VishnuChowdhary@users.noreply.github.com>
2023-02-04 12:07:28 +0530

    model for recipe app created

---
+++ recipe/serializers.py
@@ -0,0 +1,12 @@
+"""
+Serializer for recipe API.
+"""
+from rest_framework import serializers
+from core.models import Recipe
+
+class RecipeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only = ['id']
